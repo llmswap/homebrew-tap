8a2d8f7caf98071c55113252841883eff280732a
Sreenath <sreenathmmmenon@gmail.com>
2026-08-03 23:12:28 +0530

    Release llmswap 5.7.0

--- Formula/llmswap.rb
+++ Formula/llmswap.rb
@@ -1,33 +1,298 @@
 class Llmswap < Formula
   include Language::Python::Virtualenv
 
-  desc "cURL for LLMs - Universal AI SDK + CLI with multiple second brains and workspace memory"
+  desc "Ask, compare, and synthesize answers across LLM providers"
   homepage "https://github.com/sreenathmmenon/llmswap"
-  url "https://files.pythonhosted.org/packages/2b/8c/26e75f1d69b3e700ca556ce91edf7c0c77695e1e04efd9440abc3f05de3e/llmswap-5.2.0.tar.gz"
-  sha256 "eb2a6f054b521fa776be494a3ea57acf8f0289354ae6a9bfcbe699a7ce6802b6"
+  url "https://files.pythonhosted.org/packages/83/41/0b78de32065a6e43c055938e207b7442e69bfd77fb803bfed8aff44f6eb2/llmswap-5.7.0.tar.gz"
+  version "5.7.0"
+  sha256 "e44164e39b778962e00cb94e97e8710458b7e4c5c39c18e080091a5768be8894"
   license "MIT"
-  version "5.2.0"
 
+  depends_on "libyaml"
   depends_on "python@3.11"
+
+  resource "aiofiles" do
+    url "https://files.pythonhosted.org/packages/41/c3/534eac40372d8ee36ef40df62ec129bee4fdb5ad9706e58a29be53b2c970/aiofiles-25.1.0.tar.gz"
+    sha256 "a8d728f0a29de45dc521f18f07297428d56992a742f0cd2701ba86e44d23d5b2"
+  end
+
+  resource "aiohappyeyeballs" do
+    url "https://files.pythonhosted.org/packages/ce/f4/eec0465c2f67b2664688d0240b3212d5196fd89e741df67ddb81f8d35658/aiohappyeyeballs-2.7.1.tar.gz"
+    sha256 "065665c041c42a5938ed220bdcd7230f22527fbec085e1853d2402c8a3615d9d"
+  end
+
+  resource "aiohttp" do
+    url "https://files.pythonhosted.org/packages/58/d9/22ce5786ac0c1653ae8b6c23bded02c1686d11f0dbb45b31ce128e0df985/aiohttp-3.14.3.tar.gz"
+    sha256 "9491196535a88924a60afd5b5f434b5b203b6cc616250878dbdb223a8f7844bc"
+  end
+
+  resource "aiosignal" do
+    url "https://files.pythonhosted.org/packages/61/62/06741b579156360248d1ec624842ad0edf697050bbaf7c3e46394e106ad1/aiosignal-1.4.0.tar.gz"
+    sha256 "f47eecd9468083c2029cc99945502cb7708b082c232f9aca65da147157b251c7"
+  end
+
+  resource "annotated-types" do
+    url "https://files.pythonhosted.org/packages/5f/56/a8120250d128bed162cd73c76d45f6ef9991f3e068f62a8ee060afa3104a/annotated_types-0.8.0.tar.gz"
+    sha256 "13b2beaad985e05e2d6407ee4c4f35590b11f8d693a258a561055cac8f64cab7"
+  end
+
+  resource "anthropic" do
+    url "https://files.pythonhosted.org/packages/d7/10/4ca013cb166f226bd89e0aeb0fcaff94f45ddf716d4925ce89475d3c587b/anthropic-0.120.2.tar.gz"
+    sha256 "9722efc10c27a30a69f5338ddacdb35bc6a64297a4e4ba729bf83af873d5fb3a"
+  end
+
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/61/cc/a381afa6efea9f496eff839d4a6a1aed3bfafc7b3ab4b0d1b243a12573dd/anyio-4.14.2.tar.gz"
+    sha256 "cfa139f3ed1a23ee8f88a145ddb5ac7605b8bbfd8592baacd7ce3d8bb4313c7f"
+  end
+
+  resource "attrs" do
+    url "https://files.pythonhosted.org/packages/9a/8e/82a0fe20a541c03148528be8cac2408564a6c9a0cc7e9171802bc1d26985/attrs-26.1.0.tar.gz"
+    sha256 "d03ceb89cb322a8fd706d4fb91940737b6642aa36998fe130a9bc96c985eff32"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/a3/c2/24167ea9858356b47a87a50d39908bfdb72ceeefe0041586e704e5376b3a/certifi-2026.7.22.tar.gz"
+    sha256 "741e2c3b351ddf169a738da9f2c048608ff7f2c5cc02f1ebc6b118bb090d5d55"
+  end
+
+  resource "cffi" do
+    url "https://files.pythonhosted.org/packages/57/5f/ff100cae70ebe9d8df1c01a00e510e45d9adb5c1fdda84791b199141de97/cffi-2.1.0.tar.gz"
+    sha256 "efc1cdd798b1aaf39b4610bba7aad28c9bea9b910f25c784ccf9ec1fa719d1f9"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/bd/2a/23f34ec9d04624958e137efdc394888716353190e75f25dd22c7a2c7a8aa/charset_normalizer-3.4.9.tar.gz"
+    sha256 "673611bbd43f0810bec0b0f028ddeaaa501190339cac411f347ac76917c3ae7b"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/76/d4/81420972a676e8ffea40450d8c8c92943e7218a78fe9b64359836cc9876b/click-8.4.2.tar.gz"
+    sha256 "9a6cea6e60b17ebe0a44c5cc636d94f09bd66142c1cd7d8b4cd731c4917a15f6"
+  end
+
+  resource "cohere" do
+    url "https://files.pythonhosted.org/packages/4b/16/62ba386903ea854a2fce2cc2f959b9e0c8fa1d3e5699a3a523a6c691ffc9/cohere-7.0.8.tar.gz"
+    sha256 "5d9986b9b13cfe5590021474540648ff3c9e9ce4bfddc3d7f8e31acf1898c4d7"
+  end
+
+  resource "cryptography" do
+    url "https://files.pythonhosted.org/packages/de/41/6cbdcf9142d00fe82836fbb51e503e58088575cf7a0fe1dbff6695bf0840/cryptography-50.0.0.tar.gz"
+    sha256 "eeac2acb5a20ed25e0ad6d1df9891a520b78b404266b6d11778f25d5d691a6c9"
+  end
+
+  resource "distro" do
+    url "https://files.pythonhosted.org/packages/fc/f8/98eea607f65de6527f8a2e8885fc8015d3e6f5775df186e443e0964a11c3/distro-1.9.0.tar.gz"
+    sha256 "2fa77c6fd8940f116ee1d6b94a2f90b13b5ea8d019b98bc8bafdcabcdd9bdbed"
+  end
+
+  resource "docstring-parser" do
+    url "https://files.pythonhosted.org/packages/e0/4d/f332313098c1de1b2d2ff91cf2674415cc7cddab2ca1b01ae29774bd5fdf/docstring_parser-0.18.0.tar.gz"
+    sha256 "292510982205c12b1248696f44959db3cdd1740237a968ea1e2e7a900eeb2015"
+  end
+
+  resource "fastavro" do
+    url "https://files.pythonhosted.org/packages/6e/5b/ccb338db71f347e3bc031d268bf6dc41e5ead63b6997b8e72af92f05e18e/fastavro-1.12.2.tar.gz"
+    sha256 "3c79502d56cf6b76210032e1c53494ddfbc73c140bccf2ef4092b3f0825323ab"
+  end
+
+  resource "filelock" do
+    url "https://files.pythonhosted.org/packages/f6/57/3ba6e6cb097f85b855b00163d169f35365f44277df044dcf96d55b8f62a3/filelock-3.32.2.tar.gz"
+    sha256 "c33351e1f49cae33414acbc6d56784e6ecee82514ec90795da1161fc4836b5b8"
+  end
+
+  resource "frozenlist" do
+    url "https://files.pythonhosted.org/packages/2d/f5/c831fac6cc817d26fd54c7eaccd04ef7e0288806943f7cc5bbf69f3ac1f0/frozenlist-1.8.0.tar.gz"
+    sha256 "3ede829ed8d842f6cd48fc7081d7a41001a56f1f38603f9d49bf3020d59a31ad"
+  end
+
+  resource "fsspec" do
+    url "https://files.pythonhosted.org/packages/00/78/f34251dadb8f3921264a1d9b8946f5e542014ee2614b285261b4e40e6775/fsspec-2026.7.0.tar.gz"
+    sha256 "c803c40f4cf860b49dea58ee3e1c33cb9c790520e233537e1340049f89b82a88"
+  end
+
+  resource "google-auth" do
+    url "https://files.pythonhosted.org/packages/c7/33/dbc946a407401b975f0719658f18e664ece2109f79ffd1ff3bf226c205f4/google_auth-2.56.2.tar.gz"
+    sha256 "e28f103ca8091fb7012b99c44243d7366c29863713b8e34a220c3322b7a07051"
+  end
+
+  resource "google-genai" do
+    url "https://files.pythonhosted.org/packages/81/e6/ff83088427072cc9d5d21036788cf0ed08cc4906e4a5810e469553a43185/google_genai-2.16.0.tar.gz"
+    sha256 "c4c2524926001b18073db927a5d75bb7c8be7b5fd13ab507d599f51fff2284c5"
+  end
+
+  resource "groq" do
+    url "https://files.pythonhosted.org/packages/67/3e/687064918e861082cb02165872bbda31269126a108a78794dae86ddadae2/groq-1.6.0.tar.gz"
+    sha256 "c4237ecf0053ba85fd926bb31cb4b178996310474b4618867994e3e40d8e3c02"
+  end
+
+  resource "h11" do
+    url "https://files.pythonhosted.org/packages/01/ee/02a2c011bdab74c6fb3c75474d40b3052059d95df7e73351460c8588d963/h11-0.16.0.tar.gz"
+    sha256 "4e35b956cf45792e4caa5885e69fba00bdbc6ffafbfa020300e549b208ee5ff1"
+  end
+
+  resource "hf-xet" do
+    url "https://files.pythonhosted.org/packages/63/39/67be8d71f900d9a55761b6022821d6679fb56c64f1b6063d5af2c2606727/hf_xet-1.5.2.tar.gz"
+    sha256 "73044bd31bae33c984af832d19c752a0dffb67518fee9ddbd91d616e1101cf47"
+  end
+
+  resource "httpcore" do
+    url "https://files.pythonhosted.org/packages/06/94/82699a10bca87a5556c9c59b5963f2d039dbd239f25bc2a63907a05a14cb/httpcore-1.0.9.tar.gz"
+    sha256 "6e34463af53fd2ab5d807f399a9b45ea31c3dfa2276f15a2c3f00afff6e176e8"
+  end
+
+  resource "httpx" do
+    url "https://files.pythonhosted.org/packages/b1/df/48c586a5fe32a0f01324ee087459e112ebb7224f646c0b5023f5e79e9956/httpx-0.28.1.tar.gz"
+    sha256 "75e98c5f16b0f35b567856f597f06ff2270a374470a5c2392242528e3e3e42fc"
+  end
+
+  resource "huggingface-hub" do
+    url "https://files.pythonhosted.org/packages/82/db/3582597f8be0d34bd6881365a26d390854f12893eabdd62dd36de9df5a47/huggingface_hub-1.26.0.tar.gz"
+    sha256 "c8cd4e2df1ba9402f77fce9b509ec1d52debb502551789473f34016acc14e361"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/cd/63/9496c57188a2ee585e0f1db071d75089a11e98aa86eb99d9d7618fc1edce/idna-3.18.tar.gz"
+    sha256 "ffb385a7e039654cef1ab9ef32c6fafe283c0c0467bba1d9029738ce4a14a848"
+  end
+
+  resource "jiter" do
+    url "https://files.pythonhosted.org/packages/1d/1f/10936e16d8860c70698a1aa939a46aa0224813b782bce4e000e637da0b2d/jiter-0.16.0.tar.gz"
+    sha256 "7b24c3492c5f4f84a37946ad9cf504910cf6a782d6a4e0689b6673c5894b4a1c"
+  end
+
+  resource "multidict" do
+    url "https://files.pythonhosted.org/packages/1a/c2/c2d94cbe6ac1753f3fc980da97b3d930efe1da3af3c9f5125354436c073d/multidict-6.7.1.tar.gz"
+    sha256 "ec6652a1bee61c53a3e5776b6049172c53b6aaba34f18c9ad04f82712bac623d"
+  end
+
+  resource "openai" do
+    url "https://files.pythonhosted.org/packages/79/4b/87a877fd987db5737e43e357ff968e2c04f21be54034507fb38de5e4af99/openai-2.52.1.tar.gz"
+    sha256 "d8f2bf9f0ec8104b171f3448e98baa59a41e26906cf53f5343346d0fa7924d98"
+  end
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/d7/f1/e7a6dd94a8d4a5626c03e4e99c87f241ba9e350cd9e6d75123f992427270/packaging-26.2.tar.gz"
+    sha256 "ff452ff5a3e828ce110190feff1178bb1f2ea2281fa2075aadb987c2fb221661"
+  end
+
+  resource "propcache" do
+    url "https://files.pythonhosted.org/packages/ec/44/c87281c333769159c50594f22610f77398a47ccbfbbf23074e744e86f87c/propcache-0.5.2.tar.gz"
+    sha256 "01c4fc7480cd0598bb4b57022df55b9ca296da7fc5a8760bd8451a7e63a7d427"
+  end
+
+  resource "pyasn1" do
+    url "https://files.pythonhosted.org/packages/a4/9a/23310166d960def5897e91fe20e5b724601b02a22e84ba1f94232c0b7f67/pyasn1-0.6.4.tar.gz"
+    sha256 "9c447d8431c947fe4c8febc4ed9e760bc29011a5b01e5c74b67025bd9fb8ce81"
+  end
+
+  resource "pyasn1-modules" do
+    url "https://files.pythonhosted.org/packages/e9/e6/78ebbb10a8c8e4b61a59249394a4a594c1a7af95593dc933a349c8d00964/pyasn1_modules-0.4.2.tar.gz"
+    sha256 "677091de870a80aae844b1ca6134f54652fa2c8c5a52aa396440ac3106e941e6"
+  end
+
+  resource "pycparser" do
+    url "https://files.pythonhosted.org/packages/1b/7d/92392ff7815c21062bea51aa7b87d45576f649f16458d78b7cf94b9ab2e6/pycparser-3.0.tar.gz"
+    sha256 "600f49d217304a5902ac3c37e1281c9fe94e4d0489de643a9504c5cdfdfc6b29"
+  end
+
+  resource "pydantic" do
+    url "https://files.pythonhosted.org/packages/18/a5/b60d21ac674192f8ab0ba4e9fd860690f9b4a6e51ca5df118733b487d8d6/pydantic-2.13.4.tar.gz"
+    sha256 "c40756b57adaa8b1efeeced5c196f3f3b7c435f90e84ea7f443901bec8099ef6"
+  end
+
+  resource "pydantic-core" do
+    url "https://files.pythonhosted.org/packages/9d/56/921726b776ace8d8f5db44c4ef961006580d91dc52b803c489fafd1aa249/pydantic_core-2.46.4.tar.gz"
+    sha256 "62f875393d7f270851f20523dd2e29f082bcc82292d66db2b64ea71f64b6e1c1"
+  end
+
+  resource "python-dotenv" do
+    url "https://files.pythonhosted.org/packages/82/ed/0301aeeac3e5353ef3d94b6ec08bbcabd04a72018415dcb29e588514bba8/python_dotenv-1.2.2.tar.gz"
+    sha256 "2c371a91fbd7ba082c2c1dc1f8bf89ca22564a087c2c287cd9b662adde799cf3"
+  end
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
+    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/ac/c3/e2a2b89f2d3e2179abd6d00ebd70bff6273f37fb3e0cc209f48b39d00cbf/requests-2.34.2.tar.gz"
+    sha256 "f288924cae4e29463698d6d60bc6a4da69c89185ad1e0bcc4104f584e960b9ed"
+  end
+
+  resource "sniffio" do
+    url "https://files.pythonhosted.org/packages/a2/87/a6771e1546d97e7e041b6ae58d80074f81b7d5121207425c964ddf5cfdbd/sniffio-1.3.1.tar.gz"
+    sha256 "f4324edc670a0f49750a81b895f35c3adb843cca46f0530f79fc1babb23789dc"
+  end
+
+  resource "tabulate" do
+    url "https://files.pythonhosted.org/packages/46/58/8c37dea7bbf769b20d58e7ace7e5edfe65b849442b00ffcdd56be88697c6/tabulate-0.10.0.tar.gz"
+    sha256 "e2cfde8f79420f6deeffdeda9aaec3b6bc5abce947655d17ac662b126e48a60d"
+  end
+
+  resource "tenacity" do
+    url "https://files.pythonhosted.org/packages/47/c6/ee486fd809e357697ee8a44d3d69222b344920433d3b6666ccd9b374630c/tenacity-9.1.4.tar.gz"
+    sha256 "adb31d4c263f2bd041081ab33b498309a57c77f9acf2db65aadf0898179cf93a"
+  end
+
+  resource "tokenizers" do
+    url "https://files.pythonhosted.org/packages/c1/60/21f715d9faba5f5407ff759472ade058ec4a507ad62bcea47cb847239a73/tokenizers-0.23.1.tar.gz"
+    sha256 "1feeeadf865a7915adc25445dea30e9933e593c31bb96c277cee36de227c8bfa"
+  end
+
+  resource "tqdm" do
+    url "https://files.pythonhosted.org/packages/21/3b/6c24bec5be5e743ffd99576daa5cc077722fc7d5bbc00bd133fa0c698dc6/tqdm-4.70.0.tar.gz"
+    sha256 "55b0b0dbd97462d06ebee91e4dac24ed4d4702be82b24f07e6c1d27e08cea220"
+  end
+
+  resource "types-requests" do
+    url "https://files.pythonhosted.org/packages/db/51/703318f7b7be8bee126ec13bf615050f932d0179b8784420f3a0199cc769/types_requests-2.33.0.20260712.tar.gz"
+    sha256 "2141b67ab534a5c5cd2dac5034f2a35f42e699c5bf185eee608c5246a069d7fb"
+  end
+
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/f6/cc/6253133b5bb138fc3306cebfbda2c520f545d36b5be2c7255cc528bb45d6/typing_extensions-4.16.0.tar.gz"
+    sha256 "dc983d19a509c94dba722ee6abd33940f7c05a89e243c47e907eb4db6f1a43e5"
+  end
+
+  resource "typing-inspection" do
+    url "https://files.pythonhosted.org/packages/55/e3/70399cb7dd41c10ac53367ae42139cf4b1ca5f36bb3dc6c9d33acdb43655/typing_inspection-0.4.2.tar.gz"
+    sha256 "ba561c48a67c5958007083d386c3295464928b01faa735ab8547c5692e87f464"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  resource "websockets" do
+    url "https://files.pythonhosted.org/packages/21/f7/bc3a25c5ec26ce62ce487690becc2f3710bbc7b33338f005ad390db0b986/websockets-16.1.1.tar.gz"
+    sha256 "db234eda965dcce15df96bb9709f587cd87d4d52aaf0e80e2f34ec04c7670c57"
+  end
+
+  resource "yarl" do
+    url "https://files.pythonhosted.org/packages/31/33/ebe9e3d1f86c7a0b51094c0a146392045ca1631d2664889539dec8088a33/yarl-1.24.5.tar.gz"
+    sha256 "e81b83143bee16329c23db3c1b2d82b29892fcbcb849186d2f6e98a5abe9a57f"
+  end
 
   def install
     virtualenv_install_with_resources using: "python@3.11"
   end
 
   test do
-    # Test version
-    assert_match "5.2.0", shell_output("#{bin}/llmswap --version")
-    
-    # Test help command
+    assert_match "5.7.0", shell_output("#{bin}/llmswap --version")
+
     help_output = shell_output("#{bin}/llmswap --help")
-    assert_match "Universal LLM CLI", help_output
+    assert_match "Ask, compare, and synthesize answers", help_output
     assert_match "Available commands", help_output
-    
-    # Test config functionality (should work without API keys)
-    config_output = shell_output("#{bin}/llmswap config show 2>&1", 0)
+
+    best_help = shell_output("#{bin}/llmswap best --help")
+    assert_match "allow-cross-provider-sharing", best_help
+
+    config_output = shell_output("#{bin}/llmswap config show 2>&1")
     assert_match "provider:", config_output
-    
-    # Test cost comparison (should work without API keys)
+
     compare_output = shell_output("#{bin}/llmswap compare --input-tokens 100 --output-tokens 50")
     assert_match "Provider Cost Comparison", compare_output
   end
--- README.md
+++ README.md
@@ -1,115 +1,94 @@
-# llmswap Homebrew Tap
+# LLMSwap Homebrew tap
 
-Official Homebrew tap for [llmswap](https://github.com/sreenathmmenon/llmswap) - Universal AI CLI with multi-provider support, teaching features, and cost optimization.
+Official Homebrew tap for [LLMSwap](https://github.com/sreenathmmenon/llmswap):
+one interface for every LLM and one optional, cross-checked Best Answer.
 
-## Installation
+LLMSwap supports OpenAI, Anthropic, Gemini, Cohere, Perplexity, IBM watsonx,
+Groq, Ollama, xAI, and Sarvam through a shared Python SDK and CLI.
+
+## Install
 
 ```bash
 brew tap llmswap/tap
 brew install llmswap
+llmswap --version
+llmswap doctor
 ```
 
-## Quick Start
+Upgrade an existing installation:
 
-```bash
-# Get help
-llmswap --help
-
-# Basic usage (requires API keys)
-llmswap ask "Explain quantum computing in simple terms"
-
-# 🆕 NEW v5.0: Age-appropriate explanations
-llmswap ask "What is Docker?" --age 10
-llmswap ask "What is blockchain?" --audience "business owner"
-
-# 🆕 NEW v5.0: Conversational chat mode
-llmswap chat
-
-# 🆕 NEW v5.0: Teaching mode with personas
-llmswap ask "Explain Python classes" --teach --mentor developer
-
-# Configuration
-llmswap config show
-llmswap config set provider.default anthropic
-
-# Cost analysis
-llmswap costs --input-tokens 1000 --output-tokens 500
-```
-
-## Features
-
-- **8+ AI Providers**: OpenAI GPT-4o/o1, Claude, Gemini, Cohere, Perplexity, IBM watsonx, Groq, Ollama
-- **Conversational Chat**: Provider-native conversations with mid-chat provider switching
-- **Age-Appropriate Explanations**: Target responses for specific ages or audiences
-- **Teaching Personas**: 6 different mentorship styles for enhanced learning
-- **Cost Optimization**: Real-time cost tracking and provider comparison
-- **Code Generation**: GitHub Copilot CLI alternative with multi-language support
-- **Enterprise Analytics**: Usage tracking and cost analysis
-
-## Requirements
-
-- macOS 10.15+ or Linux
-- Python 3.8+ (automatically managed by Homebrew)
-- API keys for desired providers (set via environment variables)
-
-## API Key Setup
-
-```bash
-# Set environment variables for your chosen providers
-export OPENAI_API_KEY="sk-..."
-export ANTHROPIC_API_KEY="sk-ant-..."
-export GOOGLE_API_KEY="AI..."
-export GROQ_API_KEY="gsk_..."
-export COHERE_API_KEY="..."
-export PERPLEXITY_API_KEY="pplx-..."
-
-# For IBM watsonx
-export WATSONX_API_KEY="..."
-export WATSONX_PROJECT_ID="..."
-```
-
-## Why Homebrew?
-
-- ✅ **No virtualenv needed**: Global installation and access
-- ✅ **Automatic dependencies**: Homebrew manages Python and packages
-- ✅ **Easy updates**: `brew upgrade llmswap`
-- ✅ **Clean uninstall**: `brew uninstall llmswap`
-- ✅ **System integration**: Available from any directory
-
-## Alternative Installation
-
-If you prefer pip:
-```bash
-pip install llmswap
-```
-
-## Documentation
-
-- **Main Repository**: [github.com/sreenathmmenon/llmswap](https://github.com/sreenathmmenon/llmswap)
-- **PyPI Package**: [pypi.org/project/llmswap](https://pypi.org/project/llmswap/)
-- **Issues**: [github.com/sreenathmmenon/llmswap/issues](https://github.com/sreenathmmenon/llmswap/issues)
-
-## Updates
-
-To update to the latest version:
 ```bash
 brew update
 brew upgrade llmswap
 ```
 
-## Uninstall
+## Configure a provider
+
+Export at least one provider key:
 
 ```bash
-brew uninstall llmswap
-brew untap llmswap/tap  # Optional: remove the tap
+export OPENAI_API_KEY="your-key"
+# or ANTHROPIC_API_KEY, GEMINI_API_KEY, GROQ_API_KEY, XAI_API_KEY,
+# COHERE_API_KEY, PERPLEXITY_API_KEY, SARVAM_API_KEY, or WATSONX_API_KEY
 ```
 
-## Support
+Then check readiness:
 
-For issues, feature requests, or questions:
-- Create an issue: [github.com/sreenathmmenon/llmswap/issues](https://github.com/sreenathmmenon/llmswap/issues)
-- Check documentation: [Main Repository](https://github.com/sreenathmmenon/llmswap)
+```bash
+llmswap doctor
+llmswap providers
+```
 
----
+## Use LLMSwap
 
-**llmswap v5.0** - The world's first multi-provider conversational AI CLI with teaching features and cost optimization.
+```bash
+# Ask one model
+llmswap --provider openai ask "Explain this proposal in plain language"
+
+# Continue a conversation
+llmswap --provider openai chat
+
+# Synthesize three independent drafts into one cross-checked answer
+llmswap --provider openai best \
+  "Which rollout plan has the lowest operational risk?"
+
+# Machine-readable Best Answer
+llmswap --provider openai best "Assess this decision" --format json
+```
+
+Same-provider Best Answer is the default. A flow that sends candidate output to
+a different judge provider is blocked unless
+`--allow-cross-provider-sharing` is explicitly supplied.
+
+## MCP
+
+Connect to a trusted local stdio MCP server:
+
+```bash
+llmswap mcp --provider openai --command \
+  npx -y @modelcontextprotocol/server-filesystem "$HOME/Documents"
+```
+
+Local stdio is the verified MCP transport. MCP servers run with the permissions
+of your process, so grant only the filesystem and network access they need.
+
+## Browser Arena
+
+The Homebrew formula installs the core CLI. To use the optional local browser
+Arena, install the web extra with pip or uv:
+
+```bash
+python -m pip install "llmswap[web]==5.7.0"
+llmswap web
+```
+
+## Links
+
+- [Main repository](https://github.com/sreenathmmenon/llmswap)
+- [PyPI package](https://pypi.org/project/llmswap/)
+- [5.7.0 release](https://github.com/sreenathmmenon/llmswap/releases/tag/v5.7.0)
+- [Provider and model support](https://github.com/sreenathmmenon/llmswap/blob/main/MODEL_SUPPORT.md)
+- [Security and data flow](https://github.com/sreenathmmenon/llmswap/blob/main/SECURITY.md)
+- [Issues](https://github.com/sreenathmmenon/llmswap/issues)
+
+LLMSwap is open source under the MIT License.
